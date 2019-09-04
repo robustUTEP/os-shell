d66a2c83eba5107fcea17178f0a45d821e5e2aad
Daniel Cervantes <drcervantes@miners.utep.edu>
2019-09-04 16:09:05 -0600

    full path checks now use which

--- shell/testShell.sh
+++ shell/testShell.sh
@@ -7,13 +7,15 @@ myShell=$1
 
 export PS1=""			# supress prompt
 
-
+_uname=$(which uname)
+_cat=$(which cat)
+_passwd=$(which passwd)
 
 rm -f testLog.txt
 
 
 chkcmd () {
-    echo "Testing $2" 
+    echo "Testing $2"
     echo -e "$1" | bash > /tmp/t1
     echo -e "$1" | $myShell > /tmp/t2
     if diff /tmp/t1 /tmp/t2 ; then
@@ -21,19 +23,20 @@ chkcmd () {
     else
 	result=FAILED
     fi
-    echo -e "$result: $2\n  $1" | tee -a testLog.txt
+    echo -e "$result: $2" | tee -a testLog.txt
+    echo "-----------------------------------------"
 }
 
-chkcmd "/bin/uname" "no parameter, full pathname"
-chkcmd "/bin/uname \n /bin/uname" "two commands, full pathname"
-chkcmd "/bin/uname \n\n /bin/uname" "two commands, blank line in-between, full pathname"
+chkcmd "$_uname" "no parameter, full pathname"
+chkcmd "$_uname \n $_uname" "two commands, full pathname"
+chkcmd "$_uname \n\n $_uname" "two commands, blank line in-between, full pathname"
 chkcmd "uname" "no parameter, no pathname"
-chkcmd '/bin/cat /etc/passwd' "one parameter, full pathname"
-chkcmd 'cat /etc/passwd' "one parameter, no pathname"
-chkcmd 'cat /etc/passwd | sort ' "pipe"
-chkcmd 'cat /etc/passwd | sort | wc ' "2 pipes"
-chkcmd 'cat < /etc/passwd' "redirect input"
-chkcmd 'uname > /tmp/x \n cat /tmp/x' "redirect output"
+chkcmd "$_cat $_passwd" "one parameter, full pathname"
+chkcmd "cat $_passwd" "one parameter, no pathname"
+chkcmd "cat $_passwd | sort " "pipe"
+chkcmd "cat $_passwd | sort | wc " "2 pipes"
+chkcmd "cat < $_passwd" "redirect input"
+chkcmd "uname > /tmp/x \n cat /tmp/x" "redirect output"
 (echo "sleep 1" ; echo "echo 1") > /tmp/c1
 chkcmd 'bash < /tmp/c1 &\n echo 2 \n sleep 3' "background"
 chkcmd 'cd .. \n pwd' "change dir"
